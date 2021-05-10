8265b0bddc578703d42a509cbdc97cc0222add18
Kulwinder Kohli <info@iamkulwinderkohli.com>
2021-05-11 01:46:43 +0530

    - figma design completion.

--- src/entry.tsx
+++ src/entry.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import * as ui from "./hello-paperclip.pc";
 import * as monday from "./monday.pc";
+import * as card from "./card.pc";
 import * as ReactDOM from "react-dom";
 import "./Buttons/Index.tsx";
 
@@ -10,3 +11,4 @@ document.body.appendChild(mount);
 ReactDOM.render(<monday.Monday>Hello paperclip</monday.Monday>, mount);
 ReactDOM.render(<ui.Message>Hello paperclip</ui.Message>, mount);
 ReactDOM.render(<button.Button>Hello paperclip</ui.Button>, mount);
+ReactDOM.render(<card.Card>Hello paperclip</card.Card>, mount);
